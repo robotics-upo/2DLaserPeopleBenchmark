42bac590658cf35c25090233635d2ad257ea6cc4
feramozur <famozur@upo.es>
2023-05-03 10:16:28 +0200

    Add benchmarking code

---
+++ benchmark.py
@@ -0,0 +1,88 @@
+import math
+import h5py
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+from sklearn.metrics import auc
+np.tau = math.tau
+
+ASSOC_DISTANCE = 0.5 #0.3
+
+MODELS = {
+	'PeTra':         'petra_frog',
+	'PeTra*':        'petra_frog_mixedloss',
+	'LFE-Peaks':     'LFE_mixed_global_20230421145111',
+	'LFE-PPN':       'LFE_PPN_global_all3_sep_20230427145355',
+	'DROW3':         'drow_on_frog',
+	'DR-SPAAM':      'dr_spaam_on_frog',
+}
+
+fig, ax = plt.subplots(figsize=(6.0, 6.0))
+
+ax.plot([0,1],[0,1], color='gray', linewidth=0.5, linestyle='dashed')
+#ax.set_prop_cycle(color=['red', 'green', 'blue'])
+cm = plt.get_cmap('Dark2') # Set1 looks good with 5 models
+ax.set_prop_cycle(color=cm(np.linspace(0, 1, len(MODELS))))
+
+def proper_ap(recs, precs, points=11):
+	new_r = np.linspace(0.0, 1.0, num=points, endpoint=True)
+	new_p = []
+	for rec_threshold in new_r:
+		point_idxs = np.nonzero(recs >= rec_threshold)[0]
+		if len(point_idxs) > 0:
+			new_p.append(np.max(precs[point_idxs]))
+		else:
+			new_p.append(0.0)
+	new_p = np.array(new_p, dtype=np.float32)
+	#print('Recalls:   ',new_r)
+	#print('Precisions:',new_p)
+	return np.mean(new_p)
+
+def eer(recs, precs):
+	# Find the first nonzero or else (0,0) will be the EER :)
+	def first_nonzero_idx(arr):
+		return np.nonzero(arr)[0][0]
+
+	p1 = first_nonzero_idx(precs)
+	r1 = first_nonzero_idx(recs)
+	q1 = max(p1,r1)
+	idx = np.argmin(np.abs(precs[q1:] - recs[q1:]))
+	return (precs[q1+idx] + recs[q1+idx])/2  # They are often the exact same, but if not, use average.
+
+# Load PR curves and calculate metrics
+for name,prfile in MODELS.items():
+	with np.load(f'eval/{prfile}_test_out_pr_{ASSOC_DISTANCE}.npz') as f:
+		Pr_values = f['P']
+		Rc_values = f['R']
+		Th_values = f['T']
+
+	F1_values = 2 * Pr_values * Rc_values / np.clip(Pr_values + Rc_values, 1e-16, 2+1e-16)
+	AP = None #proper_ap(Rc_values, Pr_values)
+	peakF1 = np.max(F1_values)
+	EER = eer(Rc_values, Pr_values)
+
+	print(f'---- {name} ----')
+	AP = proper_ap(Rc_values, Pr_values)
+	print(f'AP:      {AP    *100.0:.1f}')
+	print(f'Peak F1: {peakF1*100.0:.1f}')
+	print(f'EER:     {EER   *100.0:.1f}')
+
+	ax.plot(Rc_values, Pr_values, label=name, linewidth=1.25, linestyle='dashed')
+
+SMALL_OFFSET=0.01
+ticks = np.linspace(start=0.0, stop=1.0, endpoint=True, num=11)
+
+ax.set_title(f'P-R Curve (d = {ASSOC_DISTANCE})')
+ax.set_ylabel('Precision')
+ax.set_xlabel('Recall')
+ax.set_aspect('equal', adjustable='box')
+ax.set_xlim(xmin=0.0-SMALL_OFFSET, xmax=1.0+SMALL_OFFSET)
+ax.set_xticks(ticks)
+ax.set_ylim(ymin=0.0-SMALL_OFFSET, ymax=1.0+SMALL_OFFSET)
+ax.set_yticks(ticks)
+ax.set_facecolor('whitesmoke')
+ax.grid()
+ax.legend(loc='lower left')
+
+fig.tight_layout(pad=.0)
+plt.show()
---
+++ calc_pr_curve.py
@@ -0,0 +1,97 @@
+import math
+import h5py
+import numpy as np
+from tqdm import tqdm
+from sklearn.metrics import auc
+np.tau = math.tau
+
+DATASET_FILE = 'frog_test.h5'
+RESULTS_FILE = 'LFE_PPN_global_all3_sep_20230427145355_test_out.npz'
+
+ASSOC_DISTANCE = 0.5 #0.3
+
+with h5py.File(DATASET_FILE, 'r') as f:
+	gt_people      = f['circles'][:,0:2]
+	gt_people_idxs = f['circle_idx'][:]
+	gt_people_nums = f['circle_num'][:]
+
+with np.load(RESULTS_FILE) as f:
+	det_people = f['people']
+	det_people = det_people.reshape((-1,3)).astype(np.float32)
+	det_people_idxs = f['idxs']
+	det_people_nums = f['nums']
+
+NUM_SCANS = gt_people_nums.shape[0]
+assert det_people_nums.shape[0] == NUM_SCANS
+
+def scan_gt_slice(i):
+	return slice(gt_people_idxs[i], gt_people_idxs[i] + gt_people_nums[i])
+
+def scan_det_slice(i):
+	return slice(det_people_idxs[i], det_people_idxs[i] + det_people_nums[i])
+
+def count_TPs(gt, det, max_distance):
+	distances = det[:,None,:] - gt[None,:,:]
+	distances = np.hypot(distances[:,:,0], distances[:,:,1])
+
+	det_gt_match = np.argmin(distances, axis=1)
+	det_gt_dist  = np.min(distances, axis=1)
+
+	TP = 0
+	matched_gt = np.zeros((gt.shape[0],), dtype=np.uint8)
+	for i_det in np.argsort(det_gt_dist, kind='stable'):
+		if det_gt_dist[i_det] > max_distance: break
+		i_gt = det_gt_match[i_det]
+		if matched_gt[i_gt]: continue
+		matched_gt[i_gt] = 1
+		TP += 1
+
+	return TP
+
+def matchup(gt, det, max_distance=ASSOC_DISTANCE):
+	out = []
+
+	# Assuming det is in descending score order
+	last_cumTP = 0
+	for i in range(det.shape[0]):
+		cumTP = count_TPs(gt, det[:i+1,1:3], max_distance)
+		TP = cumTP - last_cumTP
+		last_cumTP = cumTP
+		assert TP >= 0
+		out.append(TP)
+
+	return np.array(out, dtype=np.uint8)
+
+det_TP = np.empty((det_people.shape[0],), dtype=np.uint8)
+
+for i in tqdm(range(gt_people_idxs.shape[0])):
+	cur_gt = gt_people[gt_people_idxs[i]:gt_people_idxs[i]+gt_people_nums[i]]
+	cur_det = det_people[det_people_idxs[i]:det_people_idxs[i]+det_people_nums[i]]
+	det_TP[det_people_idxs[i]:det_people_idxs[i]+det_people_nums[i]] = matchup(cur_gt, cur_det)
+
+Pr_values = [ ]
+Rc_values = [ ]
+Th_values = [ ]
+
+num_dets = det_people.shape[0]
+det_order = np.argsort(-det_people[:,0], axis=0, kind='stable')
+det_is_boundary = det_people[det_order[1:],0] < det_people[det_order[0:num_dets-1],0]
+
+cum_TP = 0
+for i,idx in enumerate(det_order):
+	cum_TP += det_TP[idx]
+	if i==num_dets-1 or det_is_boundary[i]:
+		Pr_values.append(cum_TP / float(i+1))
+		Rc_values.append(cum_TP / float(gt_people.shape[0]))
+		Th_values.append(idx)
+
+Pr_values = np.array(Pr_values, dtype=np.float32)
+Rc_values = np.array(Rc_values, dtype=np.float32)
+Th_values = det_people[np.array(Th_values,dtype=np.uint32),0]
+
+print("Saving results...")
+np.savez_compressed(RESULTS_FILE.replace('.npz', f'_pr_{ASSOC_DISTANCE}.npz'),
+	R=Rc_values,
+	P=Pr_values,
+	T=Th_values,
+)
